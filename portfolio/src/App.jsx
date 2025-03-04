2c75f9ddbe57a4e0aef679858c8f43437f81d2dd
Andre Ramos <ralphandrer04@gmail.com>
2025-03-03 21:55:57 -0800

    added framer motion

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -5,7 +5,28 @@ import Experience from './components/Experience'
 import Projects from './components/Projects'
 import Footer from './components/Footer'
 import Contact from './components/Contact'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import PageWrapper from './components/PageWrapper'
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
+
+const AnimatedRoutes = () => {
+  const location = useLocation(); // Get current route
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/about" element={
+          <PageWrapper> <About /> </PageWrapper>} />
+        <Route path="/experience" element={
+          <PageWrapper> <Experience /> </PageWrapper >} />
+        <Route path="/projects" element={
+          <PageWrapper> <Projects /> </PageWrapper > } />
+        <Route path="/contact" element={
+          <PageWrapper> <Contact /> </PageWrapper>} />
+      </Routes>
+    </AnimatePresence>
+  );
+};
 
 const App = () => { 
   return (
@@ -15,12 +36,7 @@ const App = () => {
           max-w-screen-d : sets max width of the element
           min-h-screen ensures it takes full height*/}
       <div className="mx-auto max-w-screen-md px-6 min-h-screen"> 
-        <Routes> 
-          <Route path="/about" element={<About />} /> 
-          <Route path="/experience" element={<Experience />} /> 
-          <Route path="/projects" element={<Projects />} /> 
-          <Route path="/contact" element={<Contact />} /> 
-        </Routes>
+        <AnimatedRoutes />
         <Footer />  
       </div>
     </main> 
---
+++ portfolio/src/components/PageWrapper.jsx
@@ -0,0 +1,24 @@
+import { motion } from "framer-motion";
+import React from 'react'
+
+const pageVariants = {
+    initial: { opacity: 0, y: 50, filter:'blur(10px)'},
+    animate: { opacity: 1, y: 0, transition: { duration: 0.4 }, filter:'blur(0px)' },
+    exit: { opacity: 0, y: 50, transition: { duration: 0.4 } }
+  };
+
+const PageWrapper = ({ children }) => {
+  return (
+    <motion.div 
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      variants={pageVariants}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default PageWrapper;
+
